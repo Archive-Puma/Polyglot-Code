16fa0b19dd34b2bcdf86c537dbfb6d93e4e9933d
A Puma on a Penguin <kikefontanlorenzo@gmail.com>
2018-07-22 03:44:28 +0200

    :tongue: More languages!

--- javascript/lib/polyglot.js
+++ javascript/lib/polyglot.js
@@ -1,14 +1,55 @@
 const line = (string) => process.stdout.write(string)
+const ruby = (string) => console.log('\'' + string + '\'')
 
 const languages = {
   'python': {
     'print': console.log,
   },
-  'java': {
-    'System': {
-      'out': {
-        'println': console.log
+  'actionscript': {
+    'trace': console.log
+  },
+  'amigae': {
+    'WriteF': console.log
+  },
+  'asp': {
+    'Response': {
+      'Write': console.log
+    }
+  },
+  'b': {
+    'putschar': console.log
+  },
+  'c': {
+    'printf': line,
+    'puts': console.log
+  },
+  'c#': {
+    'Console': {
+      'println': console.log,
+      'WriteLine': console.log
+    }
+  },
+  'clipper': {
+    'Qout': console.log
+  },
+  'd': {
+    'writeln': console.log,
+    'std': {
+      'stdio': {
+        'writeln': console.log
       }
+    }
+  },
+  'dart': {
+    'print': console.log
+  },
+  'delphi': {
+    'Writeln': console.log
+  },
+  'elixir': {
+    'IO': {
+      'write': line,
+      'puts': console.log
     }
   },
   'go': {
@@ -17,37 +58,57 @@ const languages = {
       'Printf': line
     }
   },
-  'processing': {
-    'print': line,
-    'println': console.log
-  },
-  'c# / f# / vb.net': {
-    'Console': {
-      'Write': line,
-      'WriteLine': console.log
-    }
-  },
   'haskell': {
     'putStr': line,
     'putStrLn': console.log
   },
-  'pascal': {
-    'Write': line,
-    'WriteLn': console.log
+  'java': {
+    'System': {
+      'out': {
+        'println': console.log
+      }
+    }
   },
   'kotlin': {
     'println': console.log
-  },
-  'actionscript': {
-    'trace': console.log
   },
   'lua': {
     'io': {
       'write': line
     }
   },
-  'dart': {
-    'print': console.log
+  'matlab': {
+    'fprintf': line,
+    'disp': console.log
+  },
+  'nim': {
+    'echo': console.log
+  },
+  'objetive-c': {
+    'NSLog': console.log
+  },
+  'pascal': {
+    'Write': line,
+    'WriteLn': console.log
+  },
+  'pike': {
+    'write': line,
+    'writeln': console.log
+  },
+  'processing': {
+    'print': line,
+    'println': console.log
+  },
+  'purebasic': {
+    'PrintN': console.log
+  },
+  'ruby': {
+    'p': ruby
+  },
+  'unity3d': {
+    'Debug': {
+      'Log': console.log
+    }
   }
 }
 
